bf270555c8a5e0b0a2cdba45ed110cd718ae6051
Jenny Lind <jeli2308@student.miun.se>
2025-01-31 10:11:36 +0100

    Lagt till Razor-kod i navigeringen som kollar av sidans url och lägger till en aktiv klass på aktuell sida. Stylat klassen i theme.css. Även lagt till lite kod i site.js som lagrar valt tema i localStorage så att temat funkar även vid sidladdning.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,22 @@
 ﻿// Funktionalitet för att växla mellan ljust (default) och mörkt tema.
-const themeToggleButton = document.getElementById("theme-toggle");
 
+// Hämtar in element.
+const themeToggleButton = document.getElementById("theme-toggle");
+const body = document.body;
+
+// Kontrollerar om temat finns i localStorage och använder i så fall det.
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+}
+
+// Lyssnar efter klick för att växla tema.
 themeToggleButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
+
+    // Lagrar temat i localStorage.
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 });
